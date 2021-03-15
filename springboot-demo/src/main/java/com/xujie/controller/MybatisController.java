c2f2ab81eaab323fbefbe8d5b8cacb69b6e81d79
XuJie <JedJava2020@163.com>
2021-03-15 22:13:39 +0800

    springboot 集成mybatis

---
+++ springboot-demo/src/main/java/com/xujie/controller/MybatisController.java
@@ -0,0 +1,12 @@
+package com.xujie.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description:
+ * @author: JedX
+ * @create: 2021-03-15 22:04
+ */
+@RestController
+public class MybatisController {
+}
